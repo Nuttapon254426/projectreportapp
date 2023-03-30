38d8d7dfac1400efa87633c4b86740443215ec6d
Nuttapon <74868110+Nuttapon254426@users.noreply.github.com>
2023-03-30 21:19:35 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:firebase_auth_project/pages/login_register_page.dart';
+import 'package:firebase_auth_project/pages/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth_project/pages/widget_tree.dart';
@@ -45,21 +46,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: AnimatedSplashScreen(
-        splash: Image.asset(
-          "assets/images/logo.png",
-          width: 450, // set the width to 250 pixels
-          height: 450, // set the height to 250 pixels
-          fit: BoxFit.contain, // set the fit property to contain
-        ),
-        duration: 3000,
-        splashTransition: SplashTransition.fadeTransition,
-        backgroundColor: Color.fromARGB(255, 106, 182, 245),
-        nextScreen: WidgetTree(title: 'GFG'),
-      ),
-      // Container(
-      //     color: Colors.white,
-      //     child: FlutterLogo(size: MediaQuery.of(context).size.height)),
+      home: SplashScreen(),
     );
   }
 }
